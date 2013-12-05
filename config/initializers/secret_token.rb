327879d24d176e414022df8a956168819cea1d15
Stephen Lauck <stephen.lauck@gmail.com>
2013-12-05 10:54:26 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PipelineRailsApp::Application.config.secret_key_base = '144e88793f325717d690d22d5dbf5448a9b8a09ad3bb91ba9d41d9128b0611eb360a7d403ed504fd3483d5a82c539190e1ac51ec4fb696f1ef36aed6a51a8230'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PipelineRailsApp::Application.config.session_store :cookie_store, key: '_pipeline_rails_app_session'
